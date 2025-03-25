b82ad22e7a89570a57f99e73f9f1ee1f77a04979
Alexandr Shapkin <shapkin.alexandr.sergeevich@gmail.com>
2025-03-25 20:06:32 +0700

    feat:CHECK CONSTRAINT in Chat

--- models/chat_model.go
+++ models/chat_model.go
@@ -3,7 +3,7 @@ package models
 type Chat struct {
 	ID   string `gorm:"primaryKey"`
 	Name string
-	Type string `gorm:"type:enum('private','public');not null"`
+	Type string `gorm:"not null;check:type IN ('private', 'public')"`
 
 	Messages    []Message         `gorm:"foreignKey:ChatID"`
 	Paticipants []ChatPaticipants `gorm:"foreignKey:ChatID"`
