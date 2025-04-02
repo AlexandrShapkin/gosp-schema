2598b7b5546c3ebb30353bd9324d9a69591c071b
Alexandr Shapkin <shapkin.alexandr.sergeevich@gmail.com>
2025-04-02 20:35:29 +0700

    feat:add index to UserID field, add unique to RefreshToken field

--- models/token_model.go
+++ models/token_model.go
@@ -4,8 +4,8 @@ import "time"
 
 type Token struct {
 	ID           string    `gorm:"primaryKey"`
-	UserID       string    `gorm:"not null"`
-	RefreshToken string    `gorm:"not null"`
+	UserID       string    `gorm:"not null;index"`
+	RefreshToken string    `gorm:"not null;unique"`
 	ExpiresAt    time.Time `gorm:"not null"`
 
 	User User `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
